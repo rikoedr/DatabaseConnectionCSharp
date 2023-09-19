72a1594e7bc9597d817e0928ed7ffbaad260670b
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-09-19 13:38:24 +0800

    Try database connection

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+
+namespace DatabaseConnection;
+
+public class Program
+{
+    static string connectionString = "Data Source=ENRICHO;Initial Catalog=db_hr_dts;Integrated Security=True;Connect Timeout=30;Database=db_hr_dts";
+    static SqlConnection conn;
+    public static void Main(string[] args)
+    {
+        conn = new SqlConnection(connectionString);
+
+        try
+        {
+            conn.Open();
+            Console.WriteLine("Database connected");
+
+            SqlCommand command = new SqlCommand("SELECT * FROM employees", conn);
+
+            Console.WriteLine(command.ExecuteReader());
+
+            conn.Close();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Error : {ex.Message}");
+        }
+    }
+}
