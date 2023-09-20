85bf672cad21b2d887b069311640d1cdd9d4da95
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-09-20 13:39:33 +0800

    Created DBString Class

---
+++ DBString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseConnection;
+
+public class DBString {
+    public const string Connection = "Data Source=ENRICHO;Initial Catalog=db_hr_dts;Integrated Security=True;Connect Timeout=30;Database=db_hr_dts";
+}
