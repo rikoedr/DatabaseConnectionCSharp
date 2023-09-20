6ea2a4f6cf8d84c1f02dce3c461f9ad898c2b9f9
Henricho Dirganeri <131332450+rikoedr@users.noreply.github.com>
2023-09-20 17:56:29 +0800

    Created Job Class

---
+++ Job.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DatabaseConnection;
+
+public class Job
+{
+    public string ID { get; set; }
+    public string Title { get; set; }
+    public int MinSalary { get; set; }
+    public int MaxSalary { get; set; }
+
+
+
+
+    public Job()
+    {
+        this.ID = "Empty";
+        this.Title = "Empty";
+        this.MinSalary = 0;
+        this.MaxSalary = 0;
+    }
+
+    public List<Job> GetAll()
+    {
+        List<Job> jobs = new List<Job>();
+
+        using SqlConnection dbConnection = new SqlConnection(DBString.Connection);
+        using SqlCommand dbCommand = new SqlCommand();
+
+        dbCommand.Connection = dbConnection;
+        dbCommand.CommandText = "SELECT * FROM jobs";
+
+        try
+        {
+            dbConnection.Open();
+
+            using SqlDataReader reader = dbCommand.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    jobs.Add(new Job
+                    {
+                        ID = reader.GetString(0),
+                        Title = reader.GetString(1),
+                        MinSalary = reader.GetInt32(2),
+                        MaxSalary = reader.GetInt32(3)
+                    });
+                }
+
+                reader.Close();
+                dbConnection.Close();
+
+                return jobs;
+            }
+
+            reader.Close();
+            dbConnection.Close();
+
+            return jobs;
+
+        }
+        catch (Exception error)
+        {
+            Console.WriteLine("Error : " + error.Message);
+        }
+
+        return jobs;
+    }
+
+    public Job GetById(string id)
+    {
+        using SqlConnection dbConnection = new SqlConnection(DBString.Connection);
+        using SqlCommand dbCommand = new SqlCommand();
+
+        dbCommand.Connection = dbConnection;
+        dbCommand.CommandText = "SELECT * FROM jobs WHERE id = @id";
+        dbCommand.Parameters.Add(new SqlParameter("@id", id));
+
+        try
+        {
+            dbConnection.Open();
+            using SqlDataReader reader = dbCommand.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    return new Job
+                    {
+                        ID = reader.GetString(0),
+                        Title = reader.GetString(1),
+                        MinSalary = reader.GetInt32(2),
+                        MaxSalary = reader.GetInt32(3)
+                    };
+                }
+            }
+            reader.Close();
+            dbConnection.Close();
+
+            Console.WriteLine("Data dengan ID yang di masukkan tidak tersedia");
+
+            return new Job();
+        }
+        catch (Exception error)
+        {
+            Console.WriteLine($"Insertion Error : {error.Message}");
+            return new Job();
+        }
+
+    }
+
+    public string Insert(string id,string title, int minSalary, int maxSalary)
+    {
+        using SqlConnection dbConnection = new SqlConnection(DBString.Connection);
+        using SqlCommand dbCommand = new SqlCommand();
+
+        dbCommand.Connection = dbConnection;
+        dbCommand.CommandText = "INSERT INTO jobs VALUES(@id, @title, @min_salary, @max_salary)";
+        dbCommand.Parameters.Add(new SqlParameter("@id", id));
+        dbCommand.Parameters.Add(new SqlParameter("@title", title));
+        dbCommand.Parameters.Add(new SqlParameter("@min_salary", minSalary));
+        dbCommand.Parameters.Add(new SqlParameter("@max_salary", maxSalary));
+
+        try
+        {
+            dbConnection.Open();
+
+            using SqlTransaction dbTransaction = dbConnection.BeginTransaction();
+
+            try
+            {
+                dbCommand.Transaction = dbTransaction;
+                int result = dbCommand.ExecuteNonQuery();
+                dbTransaction.Commit();
+                dbConnection.Close();
+
+                return result.ToString();
+            }
+            catch (Exception error)
+            {
+                dbTransaction.Rollback();
+                return $"Transaction Error : {error.Message}";
+            }
+        }
+        catch (Exception error)
+        {
+            return $"Insertion Error : {error.Message}";
+        }
+
+    }
+
+    public string Update(string id, string title, int minSalary, int maxSalary)
+    {
+        using SqlConnection dbConnection = new SqlConnection(DBString.Connection);
+        using SqlCommand dbCommand = new SqlCommand();
+
+        dbCommand.Connection = dbConnection;
+        dbCommand.CommandText = "UPDATE jobs SET title = @title, min_salary = @min_salary, max_salary = @max_salary WHERE id = @id";
+        dbCommand.Parameters.Add(new SqlParameter("@id", id));
+        dbCommand.Parameters.Add(new SqlParameter("@title", title));
+        dbCommand.Parameters.Add(new SqlParameter("@min_salary", minSalary));
+        dbCommand.Parameters.Add(new SqlParameter("@max_salary", maxSalary));
+
+
+        try
+        {
+            dbConnection.Open();
+            using SqlTransaction dbTransaction = dbConnection.BeginTransaction();
+
+            try
+            {
+                dbCommand.Transaction = dbTransaction;
+                int result = dbCommand.ExecuteNonQuery();
+
+                dbTransaction.Commit();
+                dbConnection.Close();
+
+                return result.ToString();
+            }
+            catch (Exception error)
+            {
+                dbTransaction.Rollback();
+                return $"Transaction Error : {error.Message}";
+            }
+        }
+        catch (Exception error)
+        {
+            return $"Update Proccess : {error.Message}";
+        }
+    }
+
+    public string Delete(string id)
+    {
+        using SqlConnection dbConnection = new SqlConnection(DBString.Connection);
+        using SqlCommand dbCommand = new SqlCommand();
+
+        dbCommand.Connection = dbConnection;
+        dbCommand.CommandText = "DELETE FROM jobs WHERE id = @id";
+        dbCommand.Parameters.Add(new SqlParameter("@id", id));
+
+        try
+        {
+            dbConnection.Open();
+            using SqlTransaction dbTransaction = dbConnection.BeginTransaction();
+
+            try
+            {
+                dbCommand.Transaction = dbTransaction;
+                int result = dbCommand.ExecuteNonQuery();
+
+                dbTransaction.Commit();
+                dbConnection.Close();
+
+                return result.ToString();
+            }
+            catch (Exception error)
+            {
+                dbTransaction.Rollback();
+                return $"Transaction Error : {error.Message}";
+            }
+        }
+        catch (Exception error)
+        {
+            return $"Update Proccess : {error.Message}";
+        }
+    }
+
+}
